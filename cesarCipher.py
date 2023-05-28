cce2d0aae0f1d67316abf4cb9727fa8c64e84906
s-bayat <sajjad.bayat11@gmail.com>
2023-05-28 16:39:01 +0330

    edited

---
+++ cesarCipher.py
@@ -0,0 +1,43 @@
+from string import ascii_letters
+
+print(ascii_letters)
+
+
+def encrypt(string, key):
+    alpha = ascii_letters
+    result = ''
+
+    for ch in string:
+        if ch not in alpha:
+            result += ch
+        else:
+            new_key = (alpha.index(ch) + key) % len(alpha)
+            result += alpha[new_key]
+    return result
+
+
+print(encrypt('sajjad', 3))
+
+
+def decrypt(string, key):
+    key *= -1
+    return encrypt(string, key)
+
+
+print(decrypt("vdmmdg", 3))
+
+
+def brute_force(string):
+    alpha = ascii_letters
+    key = 1
+    result = ''
+    brute_force_data = {}
+
+    while key<= len(alpha):
+        result=decrypt(string,key)
+        brute_force_data[key]=result
+        result=''
+        key+=1
+    return brute_force_data
+
+print(brute_force("vdmmdg"))
